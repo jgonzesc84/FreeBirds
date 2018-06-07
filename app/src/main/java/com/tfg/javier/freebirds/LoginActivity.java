6bafac239e08b2fcf68b597fb135e0ae70604b46
Javier <jovotron54@gmail.com>
2018-06-07 14:01:59 +0200

    first conf firebase auth dependencies

--- app/src/main/java/com/tfg/javier/freebirds/LoginActivity.java
+++ app/src/main/java/com/tfg/javier/freebirds/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.tfg.javier.freebirds;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +13,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+      FirebaseAuth  auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            // User is signed in (getCurrentUser() will be null if not signed in)
+           /* Intent intent = Intent(this, MainActivity:class.java);
+            startActivity(intent);
+            finish();*/
+        }
     }
 
 
