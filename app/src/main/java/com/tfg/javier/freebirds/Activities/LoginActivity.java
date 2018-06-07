b6ae1d4e39c5c5cd420b95c024e94ef71a96a0ad
Javier <jovotron54@gmail.com>
2018-06-07 14:47:22 +0200

    Prepare transition splash screen to Login

---
+++ app/src/main/java/com/tfg/javier/freebirds/Activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.tfg.javier.freebirds.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tfg.javier.freebirds.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/tfg/javier/freebirds/Activities/SplashActivity.java
@@ -0,0 +1,63 @@
+package com.tfg.javier.freebirds.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.tfg.javier.freebirds.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        waitMe();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    void waitMe(){
+        Thread timer= new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    //Display for 3 seconds
+                    sleep(3000);
+                }
+                catch (InterruptedException e)
+                {
+                    // TODO: handle exception
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    FirebaseAuth  auth = FirebaseAuth.getInstance();
+
+                    if (auth.getCurrentUser() != null) {
+
+                    } else{
+                        Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(loginIntent);
+                    }
+                }
+            }
+        };
+        timer.start();
+    }
+
+
+}
